951b059b644faad9e14fc7afd46546d6dcc01fdb
Natalia Vagues <natisousantos@hotmail.com>
2022-07-31 17:26:29 -0300

    CAsting

--- src/one/digitalinnovation/basecamp/Main.java
+++ src/one/digitalinnovation/basecamp/Main.java
@@ -2,106 +2,49 @@ package one.digitalinnovation.basecamp;
 
 public class Main {
 
-    public static void main(String[] args){
+        public static void main(String[] args){
+        byte b1;
+        short s1 = 1000;
+        b1 = (byte)s1; //downcast
 
-        System.out.println("PrePos");
-        prePos();
-        System.out.println("Aritmetico");
-        aritmetico();
-        System.out.println("Atribuicao");
-        atribuicao();
-        System.out.println("Precedencia");
-        precedencia();
-    }
+        long l1;
+        int i1 = 10;
+        l1  =   i1; // upcast
 
-    private static void precedencia() {
-        int i = 10;
-        int j = 20;
-        int k = 30;
+        int i2;
+        long l2 = 1000000000000000000L;
+        i2 = (int) l2; // downcast
 
-        int a = i++ + --j * k;// Primeiro executa o posfixado 10 + 19(muda o valor de j depois subtrai (20-1) * 30 (Ai multiplica)-> 10+570 ->580
-        System.out.println("i++ + --j * k:" + a);
-        System.out.println("i:" + i);
+        int i3;
+        long l3 = 10000L;
+        i3 = (int) l3; // downcast
 
+        double d1;
+        float f1 = 10.5f;
+        d1 = f1; // upcast
 
+        float f2;
+        float f3;
+        double d2 = 10000.58d;
+        f2 = (float) d2;
+        double d3 = 10000.58888888888888888888888888888888888888888888888888888888888888d;
+        f3 = (float) d3; //downcast no segundo exemplo perda de informação
 
+        int i4;
+        float f4 = 11.5697f;
+        i4 = (int) f4;//Tranformei a parte fracionada em inteiro ( perdi infos)
 
-        int b = k/ --i % 3 + 1;//30/10%3+1 ->1 (primeiro muda o valor de i para usa lo , K se mantem, valor de u foi para 11
-        //então 30/10 = 3%3 =0+1 = 1
-
-        System.out.println("k / --1 % 3+1:" +b);
-        System.out.println("i: "+i);//10
-
-        int c  = 2;
-
-        c *= i += 5; //c=2 *i; i= i+ -> c= 2*i; i=10+5; ->c=2*15; -> c=2*15; c=30
-        // primeiro precisa achar o valor de i, i voltou a valer 10, então 2=i+5 =2*15 =30
-
-        System.out.println("c *= i +=5: " + c);
-    }
-
-    private static void atribuicao() {
-        int i = 1500;
-        short j = 15;
-        long l = 500l;
-        int k = 35;
-        float f =3.5f;
-        double d = f;
-        System.out.println("d:"+ d);
-
-        i +=5;// i = i +5
-        j -=3;// j = j -3
-        d/=2.7d;// d=d/2.7d
-        l*=3;// l=l*3
-        k%=2;//k=k%2
-
-        System.out.println("i:"+i);
-        System.out.println("j:"+j);
-        System.out.print("d:"+d);
-        System.out.println("l:"+l);
-        System.out.println("k:"+k);
-
-        i=k=j;
-
-        System.out.println("k:"+k);
-        System.out.println("i:"+i);
+                System.out.println("b1:" +b1);
+                System.out.println("l1:" +l1);
+                System.out.println("i2:" +i2);
+                System.out.println("i3:" +i3);
+                System.out.println("d1:" +d1);
+                System.out.println("f2:" +f2);
+                System.out.println("f3:" +f3);
+                System.out.println("i4:" +i4);
 
 
 
 
-    }
-
-    private static void aritmetico() {
-        int a =10;
-        int b =20;
-        int c =30;
-        int d =40;
-        int e =50;
-
-        int r1 = a+b;
-        int r2 = c-a;
-        int r3 = d*b;
-        int r4 = e/a;
-        int r5 = c%b;
-
-        System.out.println("a+b" + r1);
-        System.out.println("c-a" + r2);
-        System.out.println("d*b" + r3);
-        System.out.println("e/a" + r4);
-        System.out.println("c%b" + r5);
-    }
-
-    private static void prePos() {
-        int k = 10;
-        int i =++k;
-        int j = k--;
-        int x = k;
-
-        System.out.println("i:"+i);
-        System.out.println("j:"+j);
-        System.out.println("x:"+x);
-
-    }
-
-
+        }
 }
